e343a7eae1ef2bdce720b393126c6576b93d61fa
Ben Smith <binji@chromium.org>
2012-10-20 01:06:47 -0700

    remove unused stuff

--- smoothlife.cc
+++
@@ -1,9 +0,0 @@
-#include "smoothlife.h"
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "matrix.h"
-
--- smoothlife.h
+++
@@ -1,6 +0,0 @@
-#ifndef SMOOTHLIFE_H_
-#define SMOOTHLIFE_H_
-
-#include <pthread.h>
-
-#endif  // SMOOTHLIFE_H_
