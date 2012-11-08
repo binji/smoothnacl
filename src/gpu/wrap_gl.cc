d2ac8ed7f7b822aa3bb0c3893bd91eb9bbb4afb5
Ben Smith <binji@chromium.org>
2012-11-07 18:54:29 -0800

    wrapping gl by undefining/defining it

--- src/gpu/wrap_gl.cc
+++ src/gpu/wrap_gl.cc
@@ -11,6 +11,12 @@
 #include "gpu/gl_task.h"
 #include "gpu/task_functions.h"
 
+// Allow use of real gl symbols.
+#include "gpu/undef_gl.h"
+#undef GLES2_GET_FUN
+#define GLES2_GET_FUN(F) GLES2 ## F
+#include "gpu/define_gl.h"
+
 namespace gpu {
 namespace {
 
@@ -33,152 +39,153 @@ std::shared_ptr<T> CloneData(const void* data, size_t size) {
 
 }  // namespace
 
-void glActiveTexture(GLenum texture) {
+void wrap_glActiveTexture(GLenum texture) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&::glActiveTexture, texture)));
 }
 
-void glAttachShader(ID program, ID shader) {
+void wrap_glAttachShader(ID program, ID shader) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glAttachShader, program, shader)));
 }
 
-void glBindBuffer(GLenum target, ID buffer) {
+void wrap_glBindBuffer(GLenum target, ID buffer) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glBindBuffer, target, buffer)));
 }
 
-void glBindFramebuffer(GLenum target, ID framebuffer) {
+void wrap_glBindFramebuffer(GLenum target, ID framebuffer) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glBindFramebuffer, target, framebuffer)));
 }
 
-void glBindTexture(GLenum target, ID texture) {
+void wrap_glBindTexture(GLenum target, ID texture) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glBindTexture, target, texture)));
 }
 
-void glBufferData(GLenum target, GLsizeiptr size, const GLvoid* data,
-                  GLenum usage) {
+void wrap_glBufferData(GLenum target, GLsizeiptr size, const GLvoid* data,
+                       GLenum usage) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glBufferData, target, size,
                 std::move(CloneData<uint8_t>(data, size)), usage)));
 }
 
-void glClear(GLbitfield mask) {
+void wrap_glClear(GLbitfield mask) {
   g_task_list.Enqueue(new FunctionGLTask(std::bind(&::glClear, mask)));
 }
 
-void glClearColor(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha) {
+void wrap_glClearColor(GLclampf red, GLclampf green, GLclampf blue,
+                       GLclampf alpha) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&::glClearColor, red, green, blue, alpha)));
 }
 
-void glCompileShader(ID shader) {
+void wrap_glCompileShader(ID shader) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glCompileShader, shader)));
 }
 
-ID glCreateProgram(void) {
+ID wrap_glCreateProgram(void) {
   ID id;
   g_task_list.Enqueue(new FunctionGLTask(std::bind(&task_glCreateProgram, id)));
   return id;
 }
 
-ID glCreateShader(GLenum type) {
+ID wrap_glCreateShader(GLenum type) {
   ID id;
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glCreateShader, id, type)));
   return id;
 }
 
-void glDeleteProgram(ID program) {
+void wrap_glDeleteProgram(ID program) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glDeleteProgram, program)));
 }
 
-void glDeleteFramebuffers(GLsizei n, const ID* framebuffers) {
+void wrap_glDeleteFramebuffers(GLsizei n, const ID* framebuffers) {
   assert(n == 1);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glDeleteFramebuffer, framebuffers[0])));
 }
 
-void glDeleteShader(ID shader) {
+void wrap_glDeleteShader(ID shader) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glDeleteShader, shader)));
 }
 
-void glDeleteTextures(GLsizei n, const ID* textures) {
+void wrap_glDeleteTextures(GLsizei n, const ID* textures) {
   assert(n == 1);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glDeleteShader, textures[0])));
 }
 
-void glDrawArrays(GLenum mode, GLint first, GLsizei count) {
+void wrap_glDrawArrays(GLenum mode, GLint first, GLsizei count) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&::glDrawArrays, mode, first, count)));
 }
 
-void glEnableVertexAttribArray(Location index) {
+void wrap_glEnableVertexAttribArray(Location index) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glEnableVertexAttribArray, index)));
 }
 
-void glFramebufferTexture2D(GLenum target, GLenum attachment, GLenum textarget,
-                            ID texture, GLint level) {
+void wrap_glFramebufferTexture2D(GLenum target, GLenum attachment,
+                                 GLenum textarget, ID texture, GLint level) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glFramebufferTexture2D, target, attachment, textarget,
                 texture, level)));
 }
 
-void glGenBuffers(GLsizei n, ID* buffers) {
+void wrap_glGenBuffers(GLsizei n, ID* buffers) {
   assert(n == 1);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glGenBuffer, buffers[0])));
 }
 
-void glGenFramebuffers(GLsizei n, ID* framebuffers) {
+void wrap_glGenFramebuffers(GLsizei n, ID* framebuffers) {
   assert(n == 1);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glGenFramebuffer, framebuffers[0])));
 }
 
-void glGenTextures(GLsizei n, ID* textures) {
+void wrap_glGenTextures(GLsizei n, ID* textures) {
   assert(n == 1);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glGenTexture, textures[0])));
 }
 
-Location glGetAttribLocation(ID program, const GLchar* name) {
+Location wrap_glGetAttribLocation(ID program, const GLchar* name) {
   Location loc;
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glGetAttribLocation, loc, program, name)));
   return loc;
 }
 
-Location glGetUniformLocation(ID program, const GLchar* name) {
+Location wrap_glGetUniformLocation(ID program, const GLchar* name) {
   Location loc;
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glGetUniformLocation, loc, program, name)));
   return loc;
 }
 
-void glLinkProgram(ID program) {
+void wrap_glLinkProgram(ID program) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glLinkProgram, program)));
 }
 
-void glShaderSource(ID shader, GLsizei count, const GLchar** string,
-                    const GLint* length) {
+void wrap_glShaderSource(ID shader, GLsizei count, const GLchar** string,
+                         const GLint* length) {
   assert(length == NULL);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glShaderSource, shader, count,
                 std::move(CloneData<const char*>(string, count)), length)));
 }
 
-void glTexImage2D(GLenum target, GLint level, GLint internalformat,
-                  GLsizei width, GLsizei height, GLint border, GLenum format,
-                  GLenum type, const GLvoid* pixels) {
+void wrap_glTexImage2D(GLenum target, GLint level, GLint internalformat,
+                       GLsizei width, GLsizei height, GLint border,
+                       GLenum format, GLenum type, const GLvoid* pixels) {
   assert(type == GL_FLOAT);
   size_t bpp;
   switch (format) {
@@ -197,43 +204,43 @@ void glTexImage2D(GLenum target, GLint level, GLint internalformat,
                 std::move(CloneData<uint8_t>(pixels, size)))));
 }
 
-void glTexParameterf(GLenum target, GLenum pname, GLfloat param) {
+void wrap_glTexParameterf(GLenum target, GLenum pname, GLfloat param) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&::glTexParameterf, target, pname, param)));
 }
 
-void glUniform1f(Location location, GLfloat x) {
+void wrap_glUniform1f(Location location, GLfloat x) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glUniform1f, location, x)));
 }
 
-void glUniform1i(Location location, GLint x) {
+void wrap_glUniform1i(Location location, GLint x) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glUniform1i, location, x)));
 }
 
-void glUniformMatrix4fv(Location location, GLsizei count, GLboolean transpose,
-                        const GLfloat* value) {
+void wrap_glUniformMatrix4fv(Location location, GLsizei count,
+                             GLboolean transpose, const GLfloat* value) {
   assert(count == 1);
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glUniformMatrix4fv, location, count, transpose,
                 std::move(CloneData<float>(value, 16)))));
 }
 
-void glUseProgram(ID program) {
+void wrap_glUseProgram(ID program) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glUseProgram, program)));
 }
 
-void glVertexAttribPointer(Location indx, GLint size, GLenum type,
-                           GLboolean normalized, GLsizei stride,
-                           const GLvoid* ptr) {
+void wrap_glVertexAttribPointer(Location indx, GLint size, GLenum type,
+                                GLboolean normalized, GLsizei stride,
+                                const GLvoid* ptr) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&task_glVertexAttribPointer, indx, size, type,
                 normalized, stride, ptr)));
 }
 
-void glViewport(GLint x, GLint y, GLsizei width, GLsizei height) {
+void wrap_glViewport(GLint x, GLint y, GLsizei width, GLsizei height) {
   g_task_list.Enqueue(new FunctionGLTask(
       std::bind(&::glViewport, x, y, width, height)));
 }
--- src/gpu/wrap_gl.h
+++ src/gpu/wrap_gl.h
@@ -8,49 +8,60 @@
 #include <GLES2/gl2.h>
 #include "gpu/future.h"
 
+// First, undefine all GL calls.
+#include "gpu/undef_gl.h"
+
+// Redefine all GL calls, even though we only wrap a subset; that way if new
+// calls are added we break at compile time.
+
+#undef GLES2_GET_FUN
+#define GLES2_GET_FUN(F) wrap_gl ## F
+#include "gpu/define_gl.h"
+
 namespace gpu {
 
-void glActiveTexture(GLenum texture);
-void glAttachShader(ID program, ID shader);
-void glBindBuffer(GLenum target, ID buffer);
-void glBindFramebuffer(GLenum target, ID framebuffer);
-void glBindTexture(GLenum target, ID texture);
-void glBufferData(GLenum target, GLsizeiptr size, const GLvoid* data,
-                  GLenum usage);
-void glClear(GLbitfield mask);
-void glClearColor(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
-void glCompileShader(ID shader);
-ID glCreateProgram(void);
-ID glCreateShader(GLenum type);
-void glDeleteProgram(ID program);
-void glDeleteFramebuffers(GLsizei n, const ID* renderbuffers);
-void glDeleteShader(ID shader);
-void glDeleteTextures(GLsizei n, const ID* textures);
-void glDrawArrays(GLenum mode, GLint first, GLsizei count);
-void glEnableVertexAttribArray(Location index);
-void glFramebufferTexture2D(GLenum target, GLenum attachment, GLenum textarget,
-                            ID texture, GLint level);
-void glGenBuffers(GLsizei n, ID* buffers);
-void glGenFramebuffers(GLsizei n, ID* framebuffers);
-void glGenTextures(GLsizei n, ID* textures);
-Location glGetAttribLocation(ID program, const GLchar* name);
-Location glGetUniformLocation(ID program, const GLchar* name);
-void glLinkProgram(ID program);
-void glShaderSource(ID shader, GLsizei count, const GLchar** string,
-                    const GLint* length);
-void glTexImage2D(GLenum target, GLint level, GLint internalformat,
-                  GLsizei width, GLsizei height, GLint border, GLenum format,
-                  GLenum type, const GLvoid* pixels);
-void glTexParameterf(GLenum target, GLenum pname, GLfloat param);
-void glUniform1f(Location location, GLfloat x);
-void glUniform1i(Location location, GLint x);
-void glUniformMatrix4fv(Location location, GLsizei count, GLboolean transpose,
-                        const GLfloat* value);
-void glUseProgram(ID program);
-void glVertexAttribPointer(Location indx, GLint size, GLenum type,
-                           GLboolean normalized, GLsizei stride,
-                           const GLvoid* ptr);
-void glViewport(GLint x, GLint y, GLsizei width, GLsizei height);
+void wrap_glActiveTexture(GLenum texture);
+void wrap_glAttachShader(ID program, ID shader);
+void wrap_glBindBuffer(GLenum target, ID buffer);
+void wrap_glBindFramebuffer(GLenum target, ID framebuffer);
+void wrap_glBindTexture(GLenum target, ID texture);
+void wrap_glBufferData(GLenum target, GLsizeiptr size, const GLvoid* data,
+                       GLenum usage);
+void wrap_glClear(GLbitfield mask);
+void wrap_glClearColor(GLclampf red, GLclampf green, GLclampf blue,
+                       GLclampf alpha);
+void wrap_glCompileShader(ID shader);
+ID wrap_glCreateProgram(void);
+ID wrap_glCreateShader(GLenum type);
+void wrap_glDeleteProgram(ID program);
+void wrap_glDeleteFramebuffers(GLsizei n, const ID* renderbuffers);
+void wrap_glDeleteShader(ID shader);
+void wrap_glDeleteTextures(GLsizei n, const ID* textures);
+void wrap_glDrawArrays(GLenum mode, GLint first, GLsizei count);
+void wrap_glEnableVertexAttribArray(Location index);
+void wrap_glFramebufferTexture2D(GLenum target, GLenum attachment,
+                                 GLenum textarget, ID texture, GLint level);
+void wrap_glGenBuffers(GLsizei n, ID* buffers);
+void wrap_glGenFramebuffers(GLsizei n, ID* framebuffers);
+void wrap_glGenTextures(GLsizei n, ID* textures);
+Location wrap_glGetAttribLocation(ID program, const GLchar* name);
+Location wrap_glGetUniformLocation(ID program, const GLchar* name);
+void wrap_glLinkProgram(ID program);
+void wrap_glShaderSource(ID shader, GLsizei count, const GLchar** string,
+                         const GLint* length);
+void wrap_glTexImage2D(GLenum target, GLint level, GLint internalformat,
+                       GLsizei width, GLsizei height, GLint border,
+                       GLenum format, GLenum type, const GLvoid* pixels);
+void wrap_glTexParameterf(GLenum target, GLenum pname, GLfloat param);
+void wrap_glUniform1f(Location location, GLfloat x);
+void wrap_glUniform1i(Location location, GLint x);
+void wrap_glUniformMatrix4fv(Location location, GLsizei count,
+                             GLboolean transpose, const GLfloat* value);
+void wrap_glUseProgram(ID program);
+void wrap_glVertexAttribPointer(Location indx, GLint size, GLenum type,
+                                GLboolean normalized, GLsizei stride,
+                                const GLvoid* ptr);
+void wrap_glViewport(GLint x, GLint y, GLsizei width, GLsizei height);
 
 }  // namespace gpu
 
