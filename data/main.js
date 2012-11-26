1b0f56c341f33869bde1fa29670d5ae5a74dfe5c
Ben Smith <binji@chromium.org>
2012-11-25 16:41:04 -0800

    Hide/show buttonbar

--- data/main.js
+++ data/main.js
@@ -440,6 +440,25 @@ function setupUI() {
   $('#colors')
       .sortable({ items: '.color', handle: 'span.handle' })
       .on('sortupdate', function (e, ui) { updateGroup('palette'); });
+
+  var buttonBarTimeout;
+  $('#buttonBar').hide()
+                 .mouseover(function (e) {
+                   window.clearTimeout(buttonBarTimeout);
+                   buttonBarTimeout = 0;
+                   e.stopPropagation();
+                 });
+  $('#listener').mousemove(function (e) {
+    $('#buttonBar').show('blind', 200, function () {
+      if (buttonBarTimeout)
+        window.clearTimeout(buttonBarTimeout);
+
+      buttonBarTimeout = window.setTimeout(function () {
+        $('#buttonBar').hide('blind', 200);
+        buttonBarTimeout = 0;
+      }, 1000);
+    });
+  });
 }
 
 function makeEmbed(appendChildTo, attrs) {
