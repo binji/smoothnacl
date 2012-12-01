d404872b36dbf807f24b8904755fc8d2d667b408
Ben Smith <binji@chromium.org>
2012-12-01 10:39:40 -0800

    Refactored to image operations.

--- src/screenshot_task.cc
+++ src/screenshot_task.cc
@@ -3,11 +3,10 @@
 // found in the LICENSE file.
 
 #include "screenshot_task.h"
+#include <algorithm>
 #include <im_format_all.h>
-#include <im_image.h>
 #include <im_process_loc.h>
 #include <im_process_pnt.h>
-#include <memory>
 #include <ppapi/cpp/completion_callback.h>
 #include <ppapi/cpp/instance.h>
 #include <ppapi/cpp/var_array_buffer.h>
@@ -22,14 +21,23 @@ struct PostMessageData {
   pp::VarArrayBuffer buffer;
 };
 
-struct ImageDeleter {
-  void operator ()(imImage* image) {
-    imImageDestroy(image);
-  }
-};
-typedef std::unique_ptr<imImage, ImageDeleter> ImagePtr;
-
 }  // namespace
+
+void ImageDeleter::operator ()(imImage* image) const {
+  if (image)
+    imImageDestroy(image);
+}
+
+void FileDeleter::operator ()(imFile* file) const {
+  if (file)
+    imFileClose(file);
+}
+
+void MemoryFilenameDeleter::operator ()(imBinMemoryFileName* filename) const {
+  if (filename)
+    free(filename->buffer);
+  delete filename;
+}
 
 // TODO(binji): Move these somewhere better.
 imBinFileBase* iBinSystemFileNewFunc() {
@@ -41,27 +49,124 @@ imBinFileBase* iBinSystemFileHandleNewFunc() {
 }
 
 void imFormatRegisterInternal() {
+  imBinFileSetCurrentModule(IM_MEMFILE);
   imFormatRegisterJPEG();
   imFormatRegisterPNG();
 }
 
-ScreenshotTask::ScreenshotTask(pp::Instance* instance, AlignedUint32* buffer)
-    : instance_(instance),
-      buffer_(buffer),
-      file_(NULL),
-      filename_(NULL) {
+ReduceImageOperation::ReduceImageOperation(size_t max_length)
+    : max_length_(max_length) {
 }
 
-ScreenshotTask::~ScreenshotTask() {
-  if (filename_)
-    free(filename_->buffer);
-  delete filename_;
-  delete buffer_;
-  if (file_)
-    imFileClose(file_);
+ImagePtr ReduceImageOperation::Run(ImagePtr src) {
+  int new_width;
+  int new_height;
+  if (src->width > src->height) {
+    new_width = max_length_;
+    new_height = src->height * max_length_ / src->width;
+  } else {
+    new_width = src->width * max_length_ / src->height;
+    new_height = max_length_;
+  }
+
+  ImagePtr dst(imImageCreate(new_width, new_height, IM_RGB, IM_BYTE));
+  if (dst == NULL) {
+    printf("ReduceImageOperation: imImageCreate failed.\n");
+    return ImagePtr();
+  }
+
+  int err = imProcessReduce(src.get(), dst.get(), 1);
+  if (err == 0) {
+    printf("ReduceImageOperation: imProcessReduce failed.\n");
+    return ImagePtr();
+  }
+
+  return dst;
+}
+
+CropImageOperation::CropImageOperation(double x_scale, double y_scale,
+                                       size_t max_length)
+    : x_scale_(x_scale),
+      y_scale_(y_scale),
+      max_length_(max_length) {
+}
+
+ImagePtr CropImageOperation::Run(ImagePtr src) {
+  int new_width;
+  int new_height;
+  if (src->width > src->height) {
+    new_width = max_length_;
+    new_height = src->height * max_length_ / src->width;
+  } else {
+    new_width = src->width * max_length_ / src->height;
+    new_height = max_length_;
+  }
+
+  ImagePtr dst(imImageCreate(new_width, new_height, IM_RGB, IM_BYTE));
+  if (dst == NULL) {
+    printf("ReduceImageOperation: imImageCreate failed.\n");
+    return ImagePtr();
+  }
+
+  int x = static_cast<int>((src->width - new_width) * x_scale_);
+  int y = static_cast<int>((src->height - new_height) * y_scale_);
+  imProcessCrop(src.get(), dst.get(), x, y);
+
+  return dst;
+}
+
+BrightnessContrastImageOperation::BrightnessContrastImageOperation(
+    double brightness_shift, double contrast_factor)
+    : brightness_shift_(brightness_shift),
+      contrast_factor_(contrast_factor) {
+}
+
+ImagePtr BrightnessContrastImageOperation::Run(ImagePtr src) {
+  ImagePtr dst(imImageCreate(src->width, src->height, IM_RGB, IM_BYTE));
+  if (dst == NULL) {
+    printf("ReduceImageOperation: imImageCreate failed.\n");
+    return ImagePtr();
+  }
+
+  float params[2];
+  params[0] = brightness_shift_;
+  params[1] = contrast_factor_;
+  imProcessToneGamut(src.get(), dst.get(), IM_GAMUT_BRIGHTCONT, &params[0]);
+
+  return dst;
+}
+
+ScreenshotTask::ScreenshotTask(pp::Instance* instance, AlignedUint32* buffer)
+    : instance_(instance),
+      buffer_(buffer) {
 }
 
 void ScreenshotTask::Run(WorkerThread*) {
+  ImagePtr image = BufferToImage();
+  image = ReduceImageOperation(256).Run(std::move(image));
+  image = CropImageOperation(0.5, 0.5, 128).Run(std::move(image));
+  image = BrightnessContrastImageOperation(10, 40).Run(std::move(image));
+
+  MemoryFilenamePtr filename(new imBinMemoryFileName);
+  filename->buffer = NULL;
+  filename->size = 10 * 1024;
+  filename->reallocate = 1.5f;
+
+  FilePtr file = WriteImageToFile(filename.get(), std::move(image));
+  pp::VarArrayBuffer array_buffer = FileToArrayBuffer(filename.get(),
+                                                      std::move(file));
+
+  PostMessageData* post_message_data = new PostMessageData;
+  post_message_data->instance = instance_;
+  post_message_data->buffer = array_buffer;
+
+  pp::Module::Get()->core()->CallOnMainThread(
+      0,
+      pp::CompletionCallback(&ScreenshotTask::MainThreadRun,
+                             post_message_data));
+}
+
+ImagePtr ScreenshotTask::BufferToImage() {
   size_t width = buffer_->size().width();
   size_t height = buffer_->size().height();
   ImagePtr image(imImageCreate(
@@ -70,7 +175,7 @@ void ScreenshotTask::Run(WorkerThread*) {
       IM_BYTE));
   if (image == NULL) {
     printf("imImageCreate failed.\n");
-    return;
+    return ImagePtr();
   }
 
   uint32_t* data = buffer_->data();
@@ -87,71 +192,42 @@ void ScreenshotTask::Run(WorkerThread*) {
     static_cast<char*>(image->data[2])[dst_y * width + x] = b;
   }
 
-  ImagePtr reduced_image(imImageCreate(width / 2, height / 2, IM_RGB, IM_BYTE));
-  if (reduced_image == NULL) {
-    printf("imImageCreate failed.\n");
-    return;
-  }
+  buffer_.reset();
 
-  int err = imProcessReduce(image.get(), reduced_image.get(), 1);
-  if (err == 0) {
-    printf("imProcessReduce failed.\n");
-    return;
-  }
+  return image;
+}
 
-  ImagePtr cropped_image(imImageCreate(width / 4, height / 4, IM_RGB, IM_BYTE));
-  if (cropped_image == NULL) {
-    printf("imImageCreate failed.\n");
-    return;
-  }
-
-  imProcessCrop(reduced_image.get(), cropped_image.get(),
-                width / 8, height / 8);
-
-  float params[2];
-  params[0] = 10;  // Brightness shift.
-  params[1] = 30;  // Constrast factor.
-  imProcessToneGamut(cropped_image.get(), cropped_image.get(),
-                     IM_GAMUT_BRIGHTCONT, &params[0]);
-
-  // Write image to Jpeg memory file.
-  err;
-  if (imBinFileSetCurrentModule(IM_MEMFILE) == -1) {
-    printf("imBinMemoryFileName failed.\n");
-    return;
-  }
-
-  filename_ = new imBinMemoryFileName;
-  filename_->buffer = NULL;
-  filename_->size = 10 * 1024;
-  filename_->reallocate = 1.5f;
-
-  file_ = imFileNew(reinterpret_cast<const char*>(filename_), "JPEG", &err);
+FilePtr ScreenshotTask::WriteImageToFile(imBinMemoryFileName* filename,
+                                         ImagePtr image) {
+  int err;
+  FilePtr file(imFileNew(reinterpret_cast<const char*>(filename),
+                         "JPEG", &err));
   if (err != IM_ERR_NONE) {
     printf("imFileNew failed. Error: %d\n", err);
-    return;
+    return FilePtr();
   }
 
-  err = imFileSaveImage(file_, cropped_image.get());
+  err = imFileSaveImage(file.get(), image.get());
   if (err != IM_ERR_NONE) {
     printf("imFileSaveImage. Error: %d\n", err);
-    return;
+    return FilePtr();
   }
 
-  imBinFile* binfile = static_cast<imBinFile*>(imFileHandle(file_, 0));
+  return file;
+}
+
+pp::VarArrayBuffer ScreenshotTask::FileToArrayBuffer(
+    imBinMemoryFileName* filename,
+    FilePtr file) {
+  imBinFile* binfile = static_cast<imBinFile*>(imFileHandle(file.get(), 0));
   size_t size = imBinFileSize(binfile);
 
-  PostMessageData* post_message_data = new PostMessageData;
-  post_message_data->instance = instance_;
-  post_message_data->buffer = pp::VarArrayBuffer(size);
-  void* var_data = post_message_data->buffer.Map();
-  memcpy(var_data, filename_->buffer, size);
-  post_message_data->buffer.Unmap();
+  pp::VarArrayBuffer array_buffer(size);
+  void* var_data = array_buffer.Map();
+  memcpy(var_data, filename->buffer, size);
+  array_buffer.Unmap();
 
-  pp::Module::Get()->core()->CallOnMainThread(
-      0,
-      pp::CompletionCallback(&ScreenshotTask::MainThreadRun,
-                             post_message_data));
+  return array_buffer;
 }
 
 void ScreenshotTask::MainThreadRun(void* user_data, int32_t result) {
--- src/screenshot_task.h
+++ src/screenshot_task.h
@@ -7,28 +7,94 @@
 
 #include <im.h>
 #include <im_binfile.h>
+#include <im_image.h>
+#include <memory>
+#include <vector>
 #include "fft_allocation.h"
 #include "task.h"
 #include "worker_thread.h"
 
 namespace pp {
 class Instance;
+class VarArrayBuffer;
 }  // namespace pp
+
+struct ImageDeleter {
+  void operator ()(imImage* image) const;
+};
+typedef std::unique_ptr<imImage, ImageDeleter> ImagePtr;
+
+struct FileDeleter {
+  void operator ()(imFile* file) const;
+};
+typedef std::unique_ptr<imFile, FileDeleter> FilePtr;
+
+struct MemoryFilenameDeleter {
+  void operator ()(imBinMemoryFileName* filename) const;
+};
+typedef std::unique_ptr<imBinMemoryFileName, MemoryFilenameDeleter>
+    MemoryFilenamePtr;
+
+class ImageOperation {
+ public:
+  virtual ~ImageOperation() {}
+  virtual ImagePtr Run(ImagePtr src) = 0;
+};
+
+class ReduceImageOperation : public ImageOperation {
+ public:
+  explicit ReduceImageOperation(size_t max_length);
+  virtual ImagePtr Run(ImagePtr src);
+
+ private:
+  size_t max_length_;
+};
+
+class CropImageOperation : public ImageOperation {
+ public:
+  CropImageOperation(double x_scale, double y_scale, size_t max_length);
+  virtual ImagePtr Run(ImagePtr src);
+
+ private:
+  double x_scale_;
+  double y_scale_;
+  size_t max_length_;
+};
+
+class BrightnessContrastImageOperation : public ImageOperation {
+ public:
+  BrightnessContrastImageOperation(double brightness_shift,
+                                   double contrast_factor);
+  virtual ImagePtr Run(ImagePtr src);
+
+ private:
+  double brightness_shift_;
+  double contrast_factor_;
+};
+
+class ScreenshotConfig {
+ public:
+  ScreenshotConfig();
+  ~ScreenshotConfig();
+
+  std::vector<ImageOperation*> operations;
+};
 
 class ScreenshotTask : public Task<WorkerThread> {
  public:
   ScreenshotTask(pp::Instance* instance, AlignedUint32* buffer);
-  ~ScreenshotTask();
 
   virtual void Run(WorkerThread* thread);
 
  private:
+  ImagePtr BufferToImage();
+  FilePtr WriteImageToFile(imBinMemoryFileName* filename, ImagePtr image);
+  pp::VarArrayBuffer FileToArrayBuffer(imBinMemoryFileName* filename,
+                                       FilePtr file);
   static void MainThreadRun(void* user_data, int32_t result);
 
   pp::Instance* instance_;  // Weak.
-  AlignedUint32* buffer_;
-  imFile* file_;
-  imBinMemoryFileName* filename_;
+  std::unique_ptr<AlignedUint32> buffer_;
 
   ScreenshotTask(const ScreenshotTask&);  // Undefined.
   ScreenshotTask& operator =(const ScreenshotTask&);  // Undefined.
